0080d23cd22921a9d42162455f8c86f081783b43
40402000 <christine.reid83@yahoo.com>
2018-10-11 22:36:45 +0100

    added unit tests with nose 2

---
+++ discworld_books/tests/test.py
@@ -0,0 +1,48 @@
+import unittest
+
+from discworld_books  import app
+
+class ProjectTests(unittest.TestCase):
+
+##########################
+####setup and teardown####
+##########################
+
+#executed prior to each test
+	def setUp(self):
+		app.config['TESTING'] = True
+		app.config['DEBUG'] = False
+		self.app = app.test_client()
+
+		self.assertEquals(app.debug, False)
+
+#executed after each test
+
+	def tearDown(self):
+		pass
+
+######################
+####helper methods####
+######################
+
+#############
+####tests####
+#############
+
+	def test_main_page(self):
+		response = self.app.get('/', follow_redirects = True)
+		self.assertIn(b'Welcome to the Discworld Books App!', response.data)
+		self.assertIn(b'Chronologically', response.data)
+		self.assertIn(b'The City Watch', response.data)
+		self.assertIn(b'The Witches', response.data)
+		self.assertIn(b'Death', response.data)
+		self.assertIn(b'Moist Von Lipwig', response.data)
+		self.assertIn(b'Rincewind/The Wizards', response.data)
+		self.assertIn(b'Tiffany Aching and the Nac Mac Feegles', response.data)
+		self.assertIn(b'For Younger Readers', response.data)
+
+
+if __name__ == "__main__":
+	unittest.main()
+
+ 
