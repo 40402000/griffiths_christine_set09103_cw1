554dca9b1106b62dd084f906932dde8f33d2cc78
40402000 <christine.reid83@yahoo.com>
2018-10-15 12:42:03 +0100

    updated index template

--- discworld_books/views.py
+++ discworld_books/views.py
@@ -1,7 +1,17 @@
 from.import app
 
-from flask import render_template
+from flask import render_template, json, url_for
 
 @app.route('/')
 def index():
 	return render_template ('index.html')
+
+#def book_data():
+#	SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+#	json_url = os.path.jpoin(SITE_ROOT, "static", "data.json")
+#	data = json.load(open(json_url))
+#
+#	return book_data = (book_data[0]['title']
+
+
+
