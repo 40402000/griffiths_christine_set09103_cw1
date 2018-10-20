2dcf1ef62a61aa73ebbbaa15dcbf2fb73018078e
40402000 <christine.reid83@yahoo.com>
2018-10-20 01:36:53 +0100

    errors moved to views.py - tested

--- discworld_books/views.py
+++ discworld_books/views.py
@@ -400,3 +400,10 @@ def the_shepherds_crown():
 
 
 
+@app.errorhandler(404)
+def not_found_error(error):
+	return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+	return render_template('500.html'), 500
